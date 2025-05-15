a36726536fe7b33afd1eac0f958d9096e58796da
Asadujjaman Turag <asadujjaman717@gmail.com>
2025-05-15 13:19:58 +0600

    Basic, Hrent, Medical validation added in company

--- obj/Debug/net8.0/HrManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/HrManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HrManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+926bf2fb111aef065461f6c4665680376eef12d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("HrManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HrManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
